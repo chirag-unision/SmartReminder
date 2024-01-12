5230c9e45d16241bf79104ab11268a1218c54c8d
chirag-unision <sharmacj123@gmail.com>
2024-01-12 20:50:50 +0530

    background service implementation

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React, { useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,6 +26,7 @@ import {
 import MapView, {PROVIDER_GOOGLE, Marker, Circle} from 'react-native-maps';
 import {enableLatestRenderer} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import BackgroundService from 'react-native-background-actions';
 
 enableLatestRenderer();
 
@@ -69,6 +71,44 @@ function App(): React.JSX.Element {
     })
   }
 
+  const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
+
+  const veryIntensiveTask = async (taskDataArguments) => {
+    // Example of an infinite loop task
+    const { delay } = taskDataArguments;
+    await new Promise( async (resolve) => {
+        for (let i = 0; BackgroundService.isRunning(); i++) {
+            console.log(i);
+            await sleep(delay);
+        }
+    });
+  };
+
+  const options = {
+      taskName: 'Example',
+      taskTitle: 'ExampleTask title',
+      taskDesc: 'ExampleTask description',
+      taskIcon: {
+          name: 'ic_launcher',
+          type: 'mipmap',
+      },
+      color: '#ff00ff',
+      linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+      parameters: {
+          delay: 1000,
+      },
+  };
+
+
+  const startService= async ()=> {
+    await BackgroundService.start(veryIntensiveTask, options);
+    await BackgroundService.updateNotification({taskDesc: 'New ExampleTask description'}); // Only Android, iOS will ignore this call
+  }
+
+  const stopService= async ()=> {
+    await BackgroundService.stop();
+  }
+
   return (
     // <SafeAreaView style={backgroundStyle}>
         <View
@@ -94,6 +134,12 @@ function App(): React.JSX.Element {
                   />
                   <Circle center={mark} radius={500} strokeColor='red' fillColor='rgba(12,12,12,0.1)' />
           </MapView>
+          <Pressable onPress={startService} style={[{"backgroundColor":"green"} ]} >
+            <Text>Click here to Start</Text>
+          </Pressable>
+          <Pressable onPress={stopService} style={[{"backgroundColor":"red"} ]} >
+            <Text>Click here to Stop</Text>
+          </Pressable>
         </View>
     // </SafeAreaView>
   );
